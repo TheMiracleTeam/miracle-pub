ec2596af2bd515e50b91cabde8da43e6b9e66a7b
HKail <934932687@qq.com>
2018-07-25 15:46:52 +0800

    1、修改MiracleController控制器访问url； 2、修改数据源url及password

--- src/main/java/com/miracle/pub/controller/MiracleController.java
+++ src/main/java/com/miracle/pub/controller/MiracleController.java
@@ -5,6 +5,7 @@ import com.miracle.pub.util.SHA1;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -20,12 +21,13 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+@RequestMapping("miracle/pub")
 @Controller
 public class MiracleController {
 
     private String Token = "zhbtest";
 
-    @RequestMapping("/")
+    @RequestMapping(value = "index", method = RequestMethod.GET)
     public ModelAndView index(ModelMap modelMap) {
         return new ModelAndView("index");
     }
